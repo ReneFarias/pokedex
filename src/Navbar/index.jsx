17cd31f3cdc99dc8403e0bd316974505fab4495d
ReneFarias <renefariasfeitosadasilva@gmail.com>
2023-02-19 00:34:13 -0300

    first commit 2

--- src/Navbar/index.jsx
+++ src/Navbar/index.jsx
@@ -57,7 +57,7 @@ export default function Navbar({ pokemonFilter }) {
       <AppBar position="static" sx={{backgroundColor: "black"}}>
         <Toolbar>
           <Box display="flex" justifyContent="space-between" width="100%">
-            <Box component="img" src="/assets/pokemon-logo.png" height="3em"/>
+            <Box component="img" src="https://raw.githubusercontent.com/ReneFarias/pokedex/gh-pages/assets/pokemon-logo.png" height="3em"/>
             <Search onChange={(e)=>pokemonFilter(e.target.value)}>
             <SearchIconWrapper>
               <SearchIcon />
