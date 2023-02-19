e505b9cf95e57a9d24a820c4b49f083d822a3ab3
ReneFarias <renefariasfeitosadasilva@gmail.com>
2023-02-19 00:13:17 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import logo from './logo.svg';
+import './App.css';
+import { Home } from './pages/Home';
+import Navbar from './Navbar';
+
+function App() {
+  return <Home />;
+  
+}
+
+export default App;
